b0e2db1346eade13a8969c039d27aba067334e99
Dan-ee-wang <dan.gnaw@gmail.com>
2023-12-15 21:47:32 -0600

    landing page

--- code/awa/awa/urls.py
+++ code/awa/awa/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("authapp/", include("authapp.urls")),
+    path("", include("authapp.urls")),
     path('admin/', admin.site.urls),
 ]
